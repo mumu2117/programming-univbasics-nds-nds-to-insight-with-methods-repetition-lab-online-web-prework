6fd0d378fb6197b95d38a47bf8f55f717c536d64
algebraic-designer-6218 <jmiller@archersystems.us>
2019-12-04 12:55:41 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -40,7 +40,7 @@ def total_gross(source)
   total_gross_of_earnings = 0
    director_index = 0
   while director_index < source.size do
-  _of_directors: names provides an Array of directors names (use	    total_gross_of_earnings += gross_for_director( source[director_index] )
+  
   #	    director_index += 1
   # Visit each key (i.e. director name), look up the value in the hash	  end
   # returned by directors_totals, and add it to a running total. When done,	total_gross_of_earnings
