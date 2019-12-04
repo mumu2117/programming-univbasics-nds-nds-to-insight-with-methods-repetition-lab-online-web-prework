02228b919065a4b575bf02e4668f8118dc24c48d
algebraic-designer-6218 <jmiller@archersystems.us>
2019-12-04 12:54:22 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -37,15 +37,15 @@ end
 
 
 def total_gross(source)
-  # Write this implementation
-  #
-  # Should use methods:
-  # 1. directors_totals: returns a Hash of { dir_name => gross }
-  # 2. list_of_directors: names provides an Array of directors names (use
-  #
-  # Visit each key (i.e. director name), look up the value in the hash
-  # returned by directors_totals, and add it to a running total. When done,
-  # return the total
+  total_gross_of_earnings = 0
+  #	  director_index = 0
+  # Should use methods:	
+  # 1. directors_totals: returns a Hash of { dir_name => gross }	  while director_index < source.size do
+  # 2. list_of_directors: names provides an Array of directors names (use	    total_gross_of_earnings += gross_for_director( source[director_index] )
+  #	    director_index += 1
+  # Visit each key (i.e. director name), look up the value in the hash	  end
+  # returned by directors_totals, and add it to a running total. When done,	total_gross_of_earnings
+  
 end
 
 
